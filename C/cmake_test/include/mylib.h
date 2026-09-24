51d264d8eabc48033f6ea8c0b4b3bd6b8c2f7397
zzq <1072047735@qq.com>
2026-09-24 20:43:38 +0800

    feat(cmake_test): 添加 CMake 库与可执行文件示例

---
+++ C/cmake_test/include/mylib.h
@@ -0,0 +1,5 @@
+#ifndef _MYLIB_H_
+#define _MYLIB_H_
+#include <stdio.h>
+void mylib_function();
+#endif // _MYLIB_H_
---
+++ C/cmake_test/include/mylib2.h
@@ -0,0 +1,7 @@
+#ifndef _MYLIB2_H_
+#define _MYLIB2_H_
+#include <stdio.h>
+void mylib2_function();
+
+
+#endif // _MYLIB2_H_
